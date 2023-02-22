444472482669cb9deb9f43d58e549bf7c9f9166d
Rishi Agrawal <rishi.agrawal@corestack.io>
2023-02-22 16:17:54 +0530

    changes to var.tf

--- vars.tf
+++ vars.tf
@@ -6,9 +6,12 @@ variable "instance-temp" {
 variable "zone" {
   description = "Zone you wnat to launch the instances"
   type = string
+  source = compute.zones.list_zones
 }
 
 variable "noOfInstance" {
   description = "NO of instances to launch with this template"
+  type = number
+  default = 1
 }
 
